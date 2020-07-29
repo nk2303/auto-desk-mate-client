1295df7c6f91da27cc811d30ae254d26ec5ce96b
kailana <kailanakahawaii@gmail.com>
2020-07-28 17:58:10 -0700

    added user reducer/actions

---
+++ auto-desk-mate-client/src/components/FourOhFour.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const FourOhFour = () => {
+
+    return(
+        <div>
+            <b>
+                404 Page Not Found
+            </b>
+        </div>
+    )
+
+}
+
+export default FourOhFour
---
+++ auto-desk-mate-client/src/components/redux/reducers/userReducer.js
@@ -0,0 +1,77 @@
+const BACKEND_DOMAIN =  "https://localhost3000/api/v1";
+
+const headers = () => {
+    return {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+}
+
+const loginSignup = (apiUrl, username, password, name) => {
+    const user = {
+        user: {
+            username,
+            password, 
+            name
+        }
+    }
+    return fetch(apiUrl, {
+        method: "POST",
+        headers: headers(),
+        body: JSON.stringify(user)
+    }).then(res =>  res.json())
+    .then(res => {
+        if (res.error) {
+            return {
+                type: "LOGIN_ERROR",
+                error: res.error
+            };
+        }
+        return {
+            type: "LOGIN",
+            payload: res
+        }
+    });
+}
+
+export const login = (username, password, name) => {
+    return loginSignup(`${BACKEND_DOMAIN}/login`, username, password, name);
+}
+
+export const signup = (username, password) => {
+
+    const user = {
+        user: {
+            username,
+            password, 
+            name
+        }
+    }
+    return fetch(`${BACKEND_DOMAIN}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(user)
+    }).then(res => res.json())
+    .then(res => {
+        if (res.error) {
+            return {
+                type: "SIGNUP_ERROR",
+                error: res.error
+            };
+        }
+        return {
+            type: "SIGNUP",
+            payload: res
+        }
+    }); 
+
+}
+
+export const logout = () => {
+    return {
+        type: "LOGOUT"
+    }
+}
