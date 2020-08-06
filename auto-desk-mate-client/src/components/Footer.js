0b50c48b265672a61aaabc67ffebe387256c42d9
kailana <kailanakahawaii@gmail.com>
2020-08-05 20:49:07 -0700

    add footer

---
+++ auto-desk-mate-client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+
+function Footer(){
+
+
+    return(
+        <div className = "footer">
+            <div className = "inner">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Footer
