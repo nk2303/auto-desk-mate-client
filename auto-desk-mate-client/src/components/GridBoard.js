d0bd8c2af4114dc20e3d2b52b383f96f82d00499
nk2303 <ngankhong03@gmail.com>
2020-08-11 15:41:34 -0700

    modifying the room area accordingly to user's input

--- auto-desk-mate-client/src/components/GridBoard.js
+++ auto-desk-mate-client/src/components/GridBoard.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import { connect } from 'react-redux';
 import Col from 'react-bootstrap/Col';
 import './InteractiveArea.css';
 
-const GridBoard = () => {
+const GridBoard = ({roomInfo}) => {
+    const targetRef = useRef();
+    console.log(targetRef.current);
+    
 
+    const [roomHeight, setRoomHeight] = useState(200);
+
+    useEffect(() =>{
+        calculateHeight(roomInfo)
+    }, [roomInfo]);
+
+    const calculateHeight = (roomInfo) => {
+        const ratio = roomInfo.room_height/roomInfo.room_width;
+        setRoomHeight(targetRef.current.offsetWidth*ratio)
+        console.log(targetRef.current.offsetWidth);
+
+    }
     return(
         <Col md={9}>
             <div>Grid board</div>
-            <div className="grid-board"></div>
+            <div ref={targetRef} style={{height: roomHeight}} className="grid-board"></div>
         </Col>
     )
 }
 
-export default GridBoard
+const mapStateToProps = (store) => {
+    return {roomInfo: store.roomInfo}
+  }
+
+export default connect(mapStateToProps, null)(GridBoard)
