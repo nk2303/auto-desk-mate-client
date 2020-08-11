791bb47e320084b82a63cb00cb75487f63647d8d
kailana <kailanakahawaii@gmail.com>
2020-08-11 15:42:17 -0700

    stash

--- auto-desk-mate-client/src/components/GridBoard.js
+++ auto-desk-mate-client/src/components/GridBoard.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Col from 'react-bootstrap/Col';
 import './InteractiveArea.css';
+import { connect } from 'react-redux'
 
-const GridBoard = () => {
+const GridBoard = ({roomInfo}) => {
+
+    const input = useRef() 
+    const [roomHeight, setRoomHeight] = useState(200)
+
+    useEffect(()=> {
+        calculateHeight(roomInfo)
+    }, [roomInfo]);
+
+    const calculateHeight = (roomInfo) => {
+        const ratio = roomInfo.room_height / roomInfo.room_width
+    }
 
     return(
         <Col md={9}>
             <div>Grid board</div>
-            <div className="grid-board"></div>
+            <div style={{height: roomHeight}} className="grid-board"></div>
         </Col>
     )
 }
 
-export default GridBoard
+
+const mapStateToProps = (store) => {
+    return {roomInfo: store.roomInfo}
+}
+export default connect(mapStateToProps)(GridBoard)
--- auto-desk-mate-client/src/components/redux/reducers/roomInputReducer.js
+++ auto-desk-mate-client/src/components/redux/reducers/roomInputReducer.js
@@ -1,5 +1,5 @@
   
-const ROOM_INITIAL_STATE = {total_desks: "0", room_width: "0", room_height: "0"}
+const ROOM_INITIAL_STATE =  {total_desks: "0", room_width: "0", room_height: "0"} 
 
 export const roomInfoReducer = (state = ROOM_INITIAL_STATE, action) => {
     switch (action.type) {
